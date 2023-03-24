b599bdef63ab6bee437d52b178eacf21e46811a5
yingrushih <shihyingru@gmail.com>
2023-03-24 23:33:16 +0800

    [Add] Firebase Project Initialize

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 import 'screens/schedule_selector/schedule_selector.dart';
 
-void main() {
+void main() async{
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
