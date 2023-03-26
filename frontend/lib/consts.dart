00fe8119522e8fec4d749d3f42a0cb6e907b35c6
Ignacio <qq1179988568@gmail.com>
2023-03-27 01:41:30 +0800

    feat add some text style and color

---
+++ frontend/lib/consts.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+///使用方法例子
+///MyStyles.tripPrimary
+///MyStyles.kTextStyleH1
+///MyStyles.kTextStyleH1.copyWith(color: MyStyles.tripPrimary)
+class MyStyles {
+  //Colors
+
+  /// 橘色
+  static const Color tripPrimary = Color(0xffEA9F49);
+
+  ///棕色
+  static const Color tripSecondary = Color(0xff998167);
+
+  ///綠色
+  static const Color tripTertiary = Color(0xff778C67);
+
+  ///淺色
+  static const Color tripNeutral = Color(0xffF6F2EA);
+
+  ///灰色們
+  static const Color greyScale000000 = Color(0xff000000);
+  static const Color greyScale212121 = Color(0xff212121);
+  static const Color greyScale424242 = Color(0xff424242);
+  static const Color greyScale616161 = Color(0xff616161);
+  static const Color greyScale757575 = Color(0xff757575);
+  static const Color greyScale9E9E9E = Color(0xff9E9E9E);
+
+  ///其他
+  static const Color greyScaleE7EAEE = Color(0xffE7EAEE);
+
+//TextStyles
+  static const TextStyle kTextStyleH1 = TextStyle(
+    fontSize: 34,
+    fontWeight: FontWeight.bold,
+    letterSpacing: 0.25,
+  );
+
+  static const TextStyle kTextStyleH2 = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.bold,
+    letterSpacing: 0,
+  );
+  static const TextStyle kTextStyleH3 = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.normal,
+    letterSpacing: 0.15,
+  );
+  static const TextStyle kTextStyleSubtitle1 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.normal,
+    letterSpacing: 0.15,
+  );
+  static const TextStyle kTextStyleBody1 = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.normal,
+    letterSpacing: 0.25,
+  );
+}
--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'firebase_options.dart';
 
 import 'screens/schedule_selector/schedule_selector.dart';
 
-void main() async{
+void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
@@ -21,16 +22,10 @@ class MyApp extends StatelessWidget {
     return GetMaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
+        textTheme: GoogleFonts.notoSansTextTheme(
+          ThemeData(brightness: Brightness.light).textTheme,
+        ),
       ),
       home: const MyHomePage(title: 'Hiking Taiwan'),
     );
