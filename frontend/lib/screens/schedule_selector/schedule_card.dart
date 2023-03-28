2087f59d052b73cea3feb301ead246791e721a41
WeiChen Lin <asdfg09487@gmail.com>
2023-03-28 14:21:23 +0800

    refactor(frontend): change some type from extension

--- frontend/lib/screens/schedule_selector/schedule_card.dart
+++ frontend/lib/screens/schedule_selector/schedule_card.dart
@@ -4,19 +4,16 @@ import '../../models/schedule_model.dart';
 import '../../consts.dart';
 
 class ScheduleCard extends StatelessWidget {
-  const ScheduleCard(
-      {Key? key, this.model, this.index})
-      : super(key: key);
+  const ScheduleCard({Key? key, this.model, this.index}) : super(key: key);
 
   final ScheduleModel? model;
   final int? index;
 
   @override
   Widget build(BuildContext context) {
-    final ScheduleModel _model =
-        model ?? ScheduleModel.sample();
+    // final ScheduleModel _model = model ?? ScheduleModel.sample();
     return Card(
-      child: Container(
+      child: SizedBox(
         height: 200,
         child: Row(
           children: [
@@ -29,18 +26,17 @@ class ScheduleCard extends StatelessWidget {
   }
 
   Widget _leftSideImage() {
-    return new Expanded(
+    return Expanded(
       flex: 1,
       child: Container(
           height: 180,
           alignment: Alignment.center,
-          child: Image.asset(
-              'assets/Demo/demo.jpeg')),
+          child: Image.asset('assets/Demo/demo.jpeg')),
     );
   }
 
   Widget _rightSideInfo() {
-    return new Expanded(
+    return Expanded(
       flex: 3,
       child: Container(
         alignment: Alignment.topLeft,
@@ -51,97 +47,61 @@ class ScheduleCard extends StatelessWidget {
   }
 
   Widget _tripInfo() {
-    return new Column(
+    return Column(
         // mainAxisAlignment:
         //     MainAxisAlignment.center,
-        children: [
-          _tripInfoTitle(),
-          _tripInfoLevel()
-        ]);
+        children: [_tripInfoTitle(), _tripInfoLevel()]);
   }
 
   Widget _tripInfoTitle() {
-    return new Container(
+    return Container(
         height: 48,
         alignment: Alignment.bottomCenter,
         child: Row(children: [
-          Padding(
-              padding: const EdgeInsets.only(
-                  right: 8.0),
-              child: Text('活動名稱',
-                  style: MyStyles.kTextStyleNormal)),
-          Padding(
-              padding: const EdgeInsets.only(
-                  right: 8.0),
-              child: Text('3/20 - 3/23',
-                  style:
-                      MyStyles.kTextStyleBody1)),
-          Text('新竹市',
-              style:
-                  MyStyles.kTextStyleSubtitle1),
+          const Padding(
+              padding: EdgeInsets.only(right: 8.0),
+              child: Text('活動名稱', style: MyStyles.kTextStyleNormal)),
+          const Padding(
+              padding: EdgeInsets.only(right: 8.0),
+              child: Text('3/20 - 3/23', style: MyStyles.kTextStyleBody1)),
+          const Text('新竹市', style: MyStyles.kTextStyleSubtitle1),
           Expanded(
               child: Container(
-                  alignment:
-                      Alignment.centerRight,
+                  alignment: Alignment.centerRight,
                   height: 50,
-                  child: Padding(
-                      padding:
-                          const EdgeInsets.only(
-                              right: 8.0),
-                      child: Text('即將額滿',
-                          style: MyStyles
-                              .kTextStyleH1))))
+                  child: const Padding(
+                      padding: EdgeInsets.only(right: 8.0),
+                      child: Text('即將額滿', style: MyStyles.kTextStyleH1))))
         ]));
   }
 
   Widget _tripInfoLevel() {
-    return new Container(
+    return SizedBox(
         height: 120,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-          Container(
+        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          SizedBox(
               height: 25,
-              child: Row(
-                  crossAxisAlignment:
-                      CrossAxisAlignment.center,
-                  children: [
-                    Padding(
-                        padding:
-                            const EdgeInsets.only(
-                                right: 8.0),
-                        child: Text('進階',
-                            style: MyStyles
-                                .kTextStyleNormal)),
-                    Container(
-                      alignment: Alignment.center,
-                      child: Padding(
-                          padding:
-                              const EdgeInsets
-                                      .only(
-                                  right: 10.0,
-                                  left: 10.0),
-                          child: Text('!',
-                              style: MyStyles
-                                  .kTextStyleH2)),
-                      decoration: BoxDecoration(
-                        borderRadius:
-                            BorderRadius.circular(
-                                999),
-                        color: MyStyles
-                            .greyScaleD9D9D9,
-                      ),
-                      height: 25,
-                    ),
-                  ])),
-          Container(
-              height: 25,
-              child: Text('中級山',
-                  style: MyStyles.kTextStyleNormal)),
-          Container(
-              height: 25,
-              child: Text('百岳',
-                  style: MyStyles.kTextStyleNormal)),
+              child:
+                  Row(crossAxisAlignment: CrossAxisAlignment.center, children: [
+                const Padding(
+                    padding: const EdgeInsets.only(right: 8.0),
+                    child: Text('進階', style: MyStyles.kTextStyleNormal)),
+                Container(
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(999),
+                    color: MyStyles.greyScaleD9D9D9,
+                  ),
+                  height: 25,
+                  child: const Padding(
+                      padding: EdgeInsets.only(right: 10.0, left: 10.0),
+                      child: Text('!', style: MyStyles.kTextStyleH2)),
+                ),
+              ])),
+          const SizedBox(
+              height: 25, child: Text('中級山', style: MyStyles.kTextStyleNormal)),
+          const SizedBox(
+              height: 25, child: Text('百岳', style: MyStyles.kTextStyleNormal)),
         ]));
   }
 }
