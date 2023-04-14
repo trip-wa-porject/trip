baa9ec3cfc62aa667b2b072ed4a9431e081be412
WeiChen Lin <asdfg09487@gmail.com>
2023-04-15 03:30:08 +0800

    modify dev

--- functions/auth/index.ts
+++ functions/auth/index.ts
@@ -1,16 +1,13 @@
 import * as firebase from 'firebase-admin'
-// import { config } from 'dotenv'
-
-// config();
 
 const _needToConnectToEmulator = process.env.NODE_ENV === 'development'
 
-if (true) {
+if (_needToConnectToEmulator) {
   process.env['FIRESTORE_EMULATOR_HOST'] = 'localhost:8080'
 }
 
 const app = firebase.initializeApp(
-  true
+  _needToConnectToEmulator
     ? {
         projectId: 'wa-project-mountain-dev',
       }
