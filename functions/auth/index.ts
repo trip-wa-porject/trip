d0382d5c2c24afe19e65b3392d6be60d2b6f05c8
WeiChen Lin <asdfg09487@gmail.com>
2023-04-17 00:35:44 +0800

    escapte character

--- functions/auth/index.ts
+++ functions/auth/index.ts
@@ -7,7 +7,7 @@ const cert = {
   type: 'service_account',
   project_id: 'wa-project-mountain',
   private_key_id: '76e7fa0b741b91c63e2e718b68bdfeba8129b776',
-  private_key: process.env.SERVICE_ACCOUNT_KEY,
+  private_key: process.env.SERVICE_ACCOUNT_KEY?.replace(/\\n/g, '\n'),
   client_email: 'wa-project-mountain@appspot.gserviceaccount.com',
   client_id: '116064397620620067301',
   auth_uri: 'https://accounts.google.com/o/oauth2/auth',
